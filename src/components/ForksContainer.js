0eaeaf6c3531777a9602ba22dade83f368041c8b
Katona Ildiko-Noemi <kat.ildiko.noemi@gmail.com>
2021-10-13 14:12:27 +0300

    Fix error if the same person forked it multiple times.

--- src/components/ForksContainer.js
+++ src/components/ForksContainer.js
@@ -1,57 +1,132 @@
 import React from 'react';
 import User from "./User";
 
+// need to make a call to: https://api.github.com/gists/{GIST_ID}}/forks?per_page=3
 const FORKS = [
     {
-        "url": "https://api.github.com/gists/aa5a315d61ae9438b18d",
-        "forks_url": "https://api.github.com/gists/aa5a315d61ae9438b18d/forks",
-        "commits_url": "https://api.github.com/gists/aa5a315d61ae9438b18d/commits",
-        "id": "aa5a315d61ae9438b18d",
-        "node_id": "MDQ6R2lzdGFhNWEzMTVkNjFhZTk0MzhiMThk",
-        "git_pull_url": "https://gist.github.com/aa5a315d61ae9438b18d.git",
-        "git_push_url": "https://gist.github.com/aa5a315d61ae9438b18d.git",
-        "html_url": "https://gist.github.com/aa5a315d61ae9438b18d",
+        "url": "https://api.github.com/gists/27dbc7660dc51f56af2aa08c3d14061e",
+        "forks_url": "https://api.github.com/gists/27dbc7660dc51f56af2aa08c3d14061e/forks",
+        "commits_url": "https://api.github.com/gists/27dbc7660dc51f56af2aa08c3d14061e/commits",
+        "id": "27dbc7660dc51f56af2aa08c3d14061e",
+        "node_id": "MDQ6R2lzdDI3ZGJjNzY2MGRjNTFmNTZhZjJhYTA4YzNkMTQwNjFl",
+        "git_pull_url": "https://gist.github.com/27dbc7660dc51f56af2aa08c3d14061e.git",
+        "git_push_url": "https://gist.github.com/27dbc7660dc51f56af2aa08c3d14061e.git",
+        "html_url": "https://gist.github.com/27dbc7660dc51f56af2aa08c3d14061e",
         "files": {
-            "hello_world.rb": {
-                "filename": "hello_world.rb",
-                "type": "application/x-ruby",
-                "language": "Ruby",
-                "raw_url": "https://gist.githubusercontent.com/octocat/6cad326836d38bd3a7ae/raw/db9c55113504e46fa076e7df3a04ce592e2e86d8/hello_world.rb",
-                "size": 167
-            }
+
         },
         "public": true,
-        "created_at": "2010-04-14T02:15:15Z",
-        "updated_at": "2011-06-20T11:34:15Z",
-        "description": "Hello World Examples",
-        "comments": 1,
+        "created_at": "2018-04-07T23:55:14Z",
+        "updated_at": "2018-04-07T23:55:15Z",
+        "description": null,
+        "comments": 0,
         "user": null,
-        "comments_url": "https://api.github.com/gists/aa5a315d61ae9438b18d/comments/",
+        "comments_url": "https://api.github.com/gists/27dbc7660dc51f56af2aa08c3d14061e/comments",
         "owner": {
-            "login": "octocat",
-            "id": 1,
-            "node_id": "MDQ6VXNlcjE=",
-            "avatar_url": "https://picsum.photos/200/300",
+            "login": "j1nx0",
+            "id": 12703771,
+            "node_id": "MDQ6VXNlcjEyNzAzNzcx",
+            "avatar_url": "https://avatars.githubusercontent.com/u/12703771?v=4",
             "gravatar_id": "",
-            "url": "https://api.github.com/users/octocat",
-            "html_url": "https://github.com/octocat",
-            "followers_url": "https://api.github.com/users/octocat/followers",
-            "following_url": "https://api.github.com/users/octocat/following{/other_user}",
-            "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
-            "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
-            "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
-            "organizations_url": "https://api.github.com/users/octocat/orgs",
-            "repos_url": "https://api.github.com/users/octocat/repos",
-            "events_url": "https://api.github.com/users/octocat/events{/privacy}",
-            "received_events_url": "https://api.github.com/users/octocat/received_events",
+            "url": "https://api.github.com/users/j1nx0",
+            "html_url": "https://github.com/j1nx0",
+            "followers_url": "https://api.github.com/users/j1nx0/followers",
+            "following_url": "https://api.github.com/users/j1nx0/following{/other_user}",
+            "gists_url": "https://api.github.com/users/j1nx0/gists{/gist_id}",
+            "starred_url": "https://api.github.com/users/j1nx0/starred{/owner}{/repo}",
+            "subscriptions_url": "https://api.github.com/users/j1nx0/subscriptions",
+            "organizations_url": "https://api.github.com/users/j1nx0/orgs",
+            "repos_url": "https://api.github.com/users/j1nx0/repos",
+            "events_url": "https://api.github.com/users/j1nx0/events{/privacy}",
+            "received_events_url": "https://api.github.com/users/j1nx0/received_events",
+            "type": "User",
+            "site_admin": false
+        }
+    },
+    {
+        "url": "https://api.github.com/gists/f309dcf43284b13d3f8f8fd4091b0a20",
+        "forks_url": "https://api.github.com/gists/f309dcf43284b13d3f8f8fd4091b0a20/forks",
+        "commits_url": "https://api.github.com/gists/f309dcf43284b13d3f8f8fd4091b0a20/commits",
+        "id": "f309dcf43284b13d3f8f8fd4091b0a20",
+        "node_id": "MDQ6R2lzdGYzMDlkY2Y0MzI4NGIxM2QzZjhmOGZkNDA5MWIwYTIw",
+        "git_pull_url": "https://gist.github.com/f309dcf43284b13d3f8f8fd4091b0a20.git",
+        "git_push_url": "https://gist.github.com/f309dcf43284b13d3f8f8fd4091b0a20.git",
+        "html_url": "https://gist.github.com/f309dcf43284b13d3f8f8fd4091b0a20",
+        "files": {
+
+        },
+        "public": true,
+        "created_at": "2018-04-07T23:55:16Z",
+        "updated_at": "2018-04-07T23:55:16Z",
+        "description": null,
+        "comments": 0,
+        "user": null,
+        "comments_url": "https://api.github.com/gists/f309dcf43284b13d3f8f8fd4091b0a20/comments",
+        "owner": {
+            "login": "j1nx0",
+            "id": 12703771,
+            "node_id": "MDQ6VXNlcjEyNzAzNzcx",
+            "avatar_url": "https://avatars.githubusercontent.com/u/12703771?v=4",
+            "gravatar_id": "",
+            "url": "https://api.github.com/users/j1nx0",
+            "html_url": "https://github.com/j1nx0",
+            "followers_url": "https://api.github.com/users/j1nx0/followers",
+            "following_url": "https://api.github.com/users/j1nx0/following{/other_user}",
+            "gists_url": "https://api.github.com/users/j1nx0/gists{/gist_id}",
+            "starred_url": "https://api.github.com/users/j1nx0/starred{/owner}{/repo}",
+            "subscriptions_url": "https://api.github.com/users/j1nx0/subscriptions",
+            "organizations_url": "https://api.github.com/users/j1nx0/orgs",
+            "repos_url": "https://api.github.com/users/j1nx0/repos",
+            "events_url": "https://api.github.com/users/j1nx0/events{/privacy}",
+            "received_events_url": "https://api.github.com/users/j1nx0/received_events",
+            "type": "User",
+            "site_admin": false
+        }
+    },
+    {
+        "url": "https://api.github.com/gists/c81b9865ecbdeca2fe0e04762cddc7b4",
+        "forks_url": "https://api.github.com/gists/c81b9865ecbdeca2fe0e04762cddc7b4/forks",
+        "commits_url": "https://api.github.com/gists/c81b9865ecbdeca2fe0e04762cddc7b4/commits",
+        "id": "c81b9865ecbdeca2fe0e04762cddc7b4",
+        "node_id": "MDQ6R2lzdGM4MWI5ODY1ZWNiZGVjYTJmZTBlMDQ3NjJjZGRjN2I0",
+        "git_pull_url": "https://gist.github.com/c81b9865ecbdeca2fe0e04762cddc7b4.git",
+        "git_push_url": "https://gist.github.com/c81b9865ecbdeca2fe0e04762cddc7b4.git",
+        "html_url": "https://gist.github.com/c81b9865ecbdeca2fe0e04762cddc7b4",
+        "files": {
+
+        },
+        "public": true,
+        "created_at": "2018-04-07T23:55:17Z",
+        "updated_at": "2018-04-07T23:55:17Z",
+        "description": null,
+        "comments": 0,
+        "user": null,
+        "comments_url": "https://api.github.com/gists/c81b9865ecbdeca2fe0e04762cddc7b4/comments",
+        "owner": {
+            "login": "j1nx0",
+            "id": 12703771,
+            "node_id": "MDQ6VXNlcjEyNzAzNzcx",
+            "avatar_url": "https://avatars.githubusercontent.com/u/12703771?v=4",
+            "gravatar_id": "",
+            "url": "https://api.github.com/users/j1nx0",
+            "html_url": "https://github.com/j1nx0",
+            "followers_url": "https://api.github.com/users/j1nx0/followers",
+            "following_url": "https://api.github.com/users/j1nx0/following{/other_user}",
+            "gists_url": "https://api.github.com/users/j1nx0/gists{/gist_id}",
+            "starred_url": "https://api.github.com/users/j1nx0/starred{/owner}{/repo}",
+            "subscriptions_url": "https://api.github.com/users/j1nx0/subscriptions",
+            "organizations_url": "https://api.github.com/users/j1nx0/orgs",
+            "repos_url": "https://api.github.com/users/j1nx0/repos",
+            "events_url": "https://api.github.com/users/j1nx0/events{/privacy}",
+            "received_events_url": "https://api.github.com/users/j1nx0/received_events",
             "type": "User",
             "site_admin": false
         }
     }
-];
+]
 
 const getUserElements = (users) =>{
-    return users.map(user => <User key={user.id} avatar_url={user.avatar_url} username={user.login}/>);
+    return users.map((user,id) => <User key={`${user.id}-${id}`} avatar_url={user.avatar_url} username={user.login}/>);
 }
 
 const getUserObject = () =>{
--- src/components/User.js
+++ src/components/User.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import './User.css'
 
 function User({username, avatar_url}) {
     return (
         <div>
-            <p>Username: {username}</p>
-            <img src={avatar_url} alt={`Avatar of the user`}/>
+            <span>{username}</span> <img className={"thumbnail"} src={avatar_url} alt={`Avatar of the user`}/>
         </div>
     );
 }
